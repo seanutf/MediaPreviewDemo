53a10a36087f888c6d0207a5acb515e89e388ee7
seanutf <seanutf@gmail.com>
2022-02-18 09:43:26 +0800

    开发配置页面4

--- app/src/main/java/com/seanutf/mediapreviewdemo/MediaPreviewSettingFragment.kt
+++ app/src/main/java/com/seanutf/mediapreviewdemo/MediaPreviewSettingFragment.kt
@@ -23,6 +23,5 @@ class MediaPreviewSettingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
     }
 }
